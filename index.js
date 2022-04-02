5c589d49fb7ce928f1cbb08f433ffb2d295f0873
Sawyer Silfies <ssilfies17@gmail.com>
2022-04-01 23:35:26 -0700

    index.js logic further developed

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ db.query = util.promisify(db.query);
 const startQuestion = [
     {
         type: 'list',
-        message: 'What would you like to do?',
+        message: 'Please choose an option from the directory',
         choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role'],
         name: 'response',
         validate: function (answer) {
@@ -28,33 +28,55 @@ function askStartQuestion() {
     .prompt (startQuestion)
     .then(({response}) => {
         if (response == 'View all departments') {
-
             viewAllDepartments();
 
         } else if (response == 'View all roles') {
-
             viewAllRoles();
 
         } else if (response == 'View all employees') {
-
             viewAllEmployees();
 
         } else if (response == 'Add a department') {
-
             addDepartment();
 
         } else if (response == 'Add a role') {
-
             addRole();
 
         } else if (response == 'Add an employee') {
-
             addEmployee();
 
         } else if (response == 'Update an employee role') {
-
             updateEmployee();
         };
     });
+};
+
+
+async function viewAllDepartments() {
+    try {
+        var results = await db.query('SELECT * FROM departments;');
+
+        console.table(results);
+
+    } catch (err) {
+
+        console.error(err);
+    }
+
+    askStartQuestion();
+};
+
+async function viewAllRoles() {
+    try {
+        var results = await db.query('SELECT * FROM roles;');
+
+        console.table(results);
+
+    } catch (err) {
+
+        console.error(err);
+    }
+
+    askStartQuestion();
 };
 
