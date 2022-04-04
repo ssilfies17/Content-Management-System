8a8198356ad8939f972ce39e79f4213d07cb00a7
Sawyer Silfies <ssilfies17@gmail.com>
2022-04-03 18:58:03 -0700

    Schema debugging solved.

--- db/schema.sql
+++ db/schema.sql
@@ -25,20 +25,20 @@ CREATE TABLE roles (
 
 
 CREATE TABLE employees (
-
+    
     id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    department_id INT,
     role_id INT,
+    department_id INT,
     manager_id INT,
     FOREIGN KEY (manager_id)
     REFERENCES employees(id)
     ON DELETE SET NULL,
-    FOREIGN KEY (department_id)
-    REFERENCES departments(id)
-    ON DELETE SET NULL,
     FOREIGN KEY (role_id)
     REFERENCES roles(id)
     ON DELETE SET NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES departments(id)
+    ON DELETE SET NULL
 );
